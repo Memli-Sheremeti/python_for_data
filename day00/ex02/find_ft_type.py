289765aebda928c62527e8c280c2b4ba3e9453a9
memli <memli@iMac-de-nini.lan>
2024-12-23 23:00:27 +0100

    Day 00

--- day00/ex02/find_ft_type.py
+++ day00/ex02/find_ft_type.py
@@ -1,19 +1,29 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    format_ft_time.py                                  :+:      :+:    :+:    #
+#    find_ft_type.py                                    :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: mshereme <mshereme@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2024/12/23 20:56:52 by mshereme          #+#    #+#              #
-#    Updated: 2024/12/23 20:56:53 by mshereme         ###   ########.fr        #
+#    Created: 2024/12/23 20:56:55 by mshereme          #+#    #+#              #
+#    Updated: 2024/12/23 20:56:56 by mshereme         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-import datetime
 
-x = datetime.datetime.now()
+def all_thing_is_obj(object: any) -> int :
+    print(type(object))
+    return 42
 
 
-print("Seconds since January 1, 1970:" + f"{x.timestamp():,}" + "or" + f"{x.timestamp():.2e}","in scientific notaion\n"
-+ x.strftime("%b"), x.strftime("%d"), x.strftime("%Y"))
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+all_thing_is_obj(ft_list)
+all_thing_is_obj(ft_tuple)
+all_thing_is_obj(ft_set)
+all_thing_is_obj(ft_dict)
+all_thing_is_obj("Brian")
+all_thing_is_obj("Toto")
+print(all_thing_is_obj(10))
