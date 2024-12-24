7b70014e60fbe1c52592d30cafe3abefdcc31ba5
memli <memli@iMac-de-nini.lan>
2024-12-24 15:09:24 +0100

    Day 00

--- day00/ex05/building.py
+++ day00/ex05/building.py
@@ -6,7 +6,7 @@
 #    By: mshereme <mshereme@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/12/24 11:43:26 by mshereme          #+#    #+#              #
-#    Updated: 2024/12/24 12:40:38 by mshereme         ###   ########.fr        #
+#    Updated: 2024/12/24 15:08:10 by mshereme         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,11 +15,15 @@ import sys
 def main():
     symbol_dic = {"lower" : 0, "upper" : 0, "space" : 0, "digit" : 0, "symbol" : 0}
     try:
-        assert len(sys.argv)  == 2
+        print(len(sys.argv))
+        assert len(sys.argv)  < 3
     except AssertionError:
       print('AssertionError: more than one argument is provided')
       return
     
+    if len(sys.argv) == 1:
+        sys.argv.append(input("What is the text to count?\n"))
+
     for i in sys.argv[1]:
         if i.islower():
             symbol_dic["lower"] += 1
@@ -32,7 +36,12 @@ def main():
         else :
             symbol_dic["symbol"] += 1
     
-    print("The text contains", sum(symbol_dic.values()) - symbol_dic["space"], "characters:")
+    print("The text contains", len(sys.argv[1]), "characters:")
+    print(symbol_dic["upper"], "upper letters")
+    print(symbol_dic["lower"], "lower letters")
+    print(symbol_dic["symbol"], "punctuation marks")
+    print(symbol_dic["space"], "spaces")
+    print(symbol_dic["digit"], "digits")
     
 
 
