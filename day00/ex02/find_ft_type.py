68530cbad6c6c0faa34106cf3cadd59ee4da510c
Dow Jones <johndow@linux.de>
2025-01-09 17:56:27 +0000

    Day 00

--- day00/ex02/find_ft_type.py
+++ day00/ex02/find_ft_type.py
@@ -6,24 +6,22 @@
 #    By: mshereme <mshereme@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/12/23 20:56:55 by mshereme          #+#    #+#              #
-#    Updated: 2024/12/23 20:56:56 by mshereme         ###   ########.fr        #
+#    Updated: 2025/01/09 17:53:42 by mshereme         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 
 def all_thing_is_obj(object: any) -> int :
-    print(type(object))
+    if object.__class__ == list :
+        print("List :", type(object))
+    elif object.__class__ == tuple :
+        print("Tuple :", type(object))
+    elif object.__class__ == set :
+        print("Set :", type(object))
+    elif object.__class__ == dict :
+        print("Dict :", type(object))
+    elif object.__class__ == str :
+        print(object, "is in the kitchen :", type(object))
+    else :
+        print("Type not found")
     return 42
-
-
-ft_list = ["Hello", "tata!"]
-ft_tuple = ("Hello", "toto!")
-ft_set = {"Hello", "tutu!"}
-ft_dict = {"Hello" : "titi!"}
-all_thing_is_obj(ft_list)
-all_thing_is_obj(ft_tuple)
-all_thing_is_obj(ft_set)
-all_thing_is_obj(ft_dict)
-all_thing_is_obj("Brian")
-all_thing_is_obj("Toto")
-print(all_thing_is_obj(10))
