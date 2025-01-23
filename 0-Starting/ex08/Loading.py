edd0d28c447a2d5750e09dab99eb47ad5f69b258
Memli-Sheremeti <memli.sh.pro@gmail.com>
2025-01-23 02:50:44 +0000

    Day 00

--- 0-Starting/ex08/Loading.py
+++ 0-Starting/ex08/Loading.py
@@ -1,19 +1,21 @@
 import sys
-import time
+from time import sleep
+from tqdm import tqdm
 
 
 def ft_tqdm(lst: range) -> None:
     print(type(lst))
     print(lst)
-    for i in lst:
-        sys.stdout.write(f"\rProgress: {i + 1}/{max(lst) + 1}")
-        sys.stdout.flush()
-        time.sleep(0.05)
-    return
+    sys.stdout.write(f"\rProgress: {1}/{max(lst) + 1}")
+    sys.stdout.flush()
+    print(iter(lst))
+    sleep(0.05)
+    return 
 
 
 def main():
-    ft_tqdm(range(100))
+    for elem in ft_tqdm(range(100)):
+        sleep
     return
 
 
