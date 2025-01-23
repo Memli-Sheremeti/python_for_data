f55b8dba10e5392f771509198c95f401b269f8d5
Memli-Sheremeti <memli.sh.pro@gmail.com>
2025-01-23 02:50:44 +0000

    Day 00

--- day00/ex05/building.py
+++ day00/ex05/building.py
@@ -6,44 +6,58 @@
 #    By: mshereme <mshereme@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/12/24 11:43:26 by mshereme          #+#    #+#              #
-#    Updated: 2025/01/09 19:25:11 by mshereme         ###   ########.fr        #
+#    Updated: 2025/01/09 20:09:34 by mshereme         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
+import string
+
+def prompte_user():
+    try :
+        user_input = input("What is the text to count?\n")
+        sys.argv.append(user_input)
+    except EOFError:
+        sys.argv.append("")
+        return 
+     
+def print_the_ouput(object: any):
+    print("The text contains", len(sys.argv[1]), "characters:")
+    print(object["upper"], "upper letters")
+    print(object["lower"], "lower letters")
+    print(object["punctuation"], "punctuation marks")
+    print(object["space"], "spaces")
+    print(object["digit"], "digits")
+
+def calcul_caracteres(object: any):
+     for i in sys.argv[1]:
+        if i.islower():
+            object["lower"] += 1
+        elif i.isupper():
+            object["upper"] += 1
+        elif i.isdigit():
+            object["digit"] += 1
+        elif i.isspace():
+            object["space"] += 1
+        elif i in string.punctuation :
+            object["punctuation"] += 1
+        else :
+            continue 
 
 def main():
-    symbol_dic = {"lower" : 0, "upper" : 0, "space" : 0, "digit" : 0, "symbol" : 0}
+    symbol_dic = {"lower" : 0, "upper" : 0, "space" : 0, "digit" : 0, "punctuation" : 0}
+    
     try:
-        print(len(sys.argv))
-        assert len(sys.argv)  < 3
-    except AssertionError:
-      print('AssertionError: more than one argument is provided')
+        assert len(sys.argv)  < 3, "more than one argument is provided"
+    except AssertionError as e:
+      print(f"AssertionError: {e}")
       return
-    
+
     if len(sys.argv) == 1:
-        sys.argv.append(input("What is the text to count?\n"))
+       prompte_user()
 
-    for i in sys.argv[1]:
-        if i.islower():
-            symbol_dic["lower"] += 1
-        elif i.isupper():
-            symbol_dic["upper"] += 1
-        elif i.isdigit():
-            symbol_dic["digit"] += 1
-        elif i.isspace():
-            symbol_dic["space"] += 1
-        else :
-            symbol_dic["symbol"] += 1
-    
-    print("The text contains", len(sys.argv[1]), "characters:")
-    print(symbol_dic["upper"], "upper letters")
-    print(symbol_dic["lower"], "lower letters")
-    print(symbol_dic["symbol"], "punctuation marks")
-    print(symbol_dic["space"], "spaces")
-    print(symbol_dic["digit"], "digits")
-    
-
+    calcul_caracteres(symbol_dic)
+    print_the_ouput(symbol_dic)
 
 if __name__ == "__main__":
     main()
