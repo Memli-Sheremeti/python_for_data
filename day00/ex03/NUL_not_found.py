4bc520cfb3ce8854625a2142d447ad2dfe49a754
Memli-Sheremeti <memli.sh.pro@gmail.com>
2025-01-23 02:50:44 +0000

    Day 00

---
+++ day00/ex03/NUL_not_found.py
@@ -0,0 +1,39 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    NUL_not_found.py                                   :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: mshereme <mshereme@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/12/23 20:55:47 by memli             #+#    #+#              #
+#    Updated: 2024/12/23 22:58:42 by mshereme         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# from NULL_not_found import NULL_not_found
+
+def NULL_not_found(object: any = None) -> int:
+    for name, value in globals().items():
+        # print(name, " | ", value)
+        if not name.startswith("__") and value is object:
+            print(name,"= ", value, type(object))
+            return 0
+    if object is None:
+        return 1
+    print("Type not Found")
+    return 1
+
+
+Nothing = None
+Garlic = float("NaN")
+Zero = 0
+Empty = ""
+Fake = False
+NULL_not_found(Nothing)
+NULL_not_found(Garlic)
+NULL_not_found(Zero)
+NULL_not_found(Empty)
+NULL_not_found(Fake)
+print(NULL_not_found("Brian"))
+
+# NULL_not_found()
