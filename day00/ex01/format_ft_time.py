a1069a20f601abf5f87c79486ce59af58ee1e76b
Memli-Sheremeti <memli.sh.pro@gmail.com>
2025-01-23 02:50:44 +0000

    Day 00

--- day00/ex01/format_ft_time.py
+++ day00/ex01/format_ft_time.py
@@ -6,14 +6,17 @@
 #    By: mshereme <mshereme@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/12/23 20:56:52 by mshereme          #+#    #+#              #
-#    Updated: 2024/12/23 20:56:53 by mshereme         ###   ########.fr        #
+#    Updated: 2025/01/09 19:19:57 by mshereme         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import datetime
 
-x = datetime.datetime.now()
+def main() :
+    x = datetime.datetime.now()
+    
+    print("Seconds since January 1, 1970:" + f"{x.timestamp():,}" + "or" + f"{x.timestamp():.2e}",
+    "in scientific notaion\n" + x.strftime("%b"), x.strftime("%d"), x.strftime("%Y"))    
 
-
-print("Seconds since January 1, 1970:" + f"{x.timestamp():,}" + "or" + f"{x.timestamp():.2e}","in scientific notaion\n"
-+ x.strftime("%b"), x.strftime("%d"), x.strftime("%Y"))
+if __name__ == "__main__" :
+    main()
