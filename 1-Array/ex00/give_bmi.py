c5469f0945055d838c682ab3b58f9ee47e4839f2
Memli-Sheremeti <memli.sh.pro@gmail.com>
2025-01-23 02:50:45 +0000

    Day 01

---
+++ 1-Array/ex00/give_bmi.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    
+    return 
+
+def main():
+    return 
+
+
+if __name__ == "__main__":
+    main()
