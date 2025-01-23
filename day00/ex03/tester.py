f9a9133d0629fd2f26883c0c360fc68fc40ac230
Memli-Sheremeti <memli.sh.pro@gmail.com>
2025-01-23 02:50:44 +0000

    Day 00

---
+++ day00/ex03/tester.py
@@ -0,0 +1,26 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    tester.py                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: mshereme <mshereme@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/01/09 17:57:48 by mshereme          #+#    #+#              #
+#    Updated: 2025/01/09 18:39:30 by mshereme         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+from NUL_not_found import NULL_not_found
+
+Nothing = None
+Garlic = float("NaN")
+Zero = 0
+Empty = ''
+Fake = False
+
+NULL_not_found(Nothing)
+NULL_not_found(Garlic)
+NULL_not_found(Zero)
+NULL_not_found(Empty)
+NULL_not_found(Fake)
+print(NULL_not_found("Brian"))
